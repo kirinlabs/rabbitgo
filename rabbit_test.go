d3a61263f660b44d503653e63db7fc16479114ec
jason <flyfreelyws@126.com>
2021-06-10 15:41:08 +0800

    add test

--- rabbit_test.go
+++ rabbit_test.go
@@ -26,10 +26,53 @@ func init() {
 */
 func TestSend(t *testing.T) {
 	ch, err := rabbit.Get()
-	defer rabbit.Push(ch)
 	if err != nil {
 		t.Logf("Get channel error, %s", err.Error())
 	}
+	defer rabbit.Push(ch)
+
+	qu, err := ch.Ch.QueueDeclare(queue, true, false, false, false, nil)
+	if err != nil {
+		t.Logf("Queue declare error, %s", err.Error())
+	}
+
+	data := fmt.Sprintf("{\"code\":200,\"message\":\"success\",\"data\":\"%s\"}", time.Now().String())
+	err = ch.Ch.Publish(
+		"",
+		qu.Name,
+		false,
+		false,
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "appliction/plain",
+			Body:         []byte(data),
+		},
+	)
+	if err != nil {
+		t.Logf("Send message error, %s", err.Error())
+	}
+}
+
+func TestSendConfirm(t *testing.T) {
+	ch, err := rabbit.Get()
+	if err != nil {
+		t.Logf("Get channel error, %s", err.Error())
+	}
+	defer rabbit.Push(ch)
+
+	ch.Ch.Confirm(false)
+	confirms := make(chan amqp.Confirmation)
+	ch.Ch.NotifyPublish(confirms)
+
+	defer func() {
+		if confirm := <-confirms; confirm.Ack {
+			// code when messages is confirmed
+			t.Logf("Confirmed tag %d", confirm.DeliveryTag)
+		} else {
+			// code when messages is nacked
+			t.Logf("Nacked tag %d", confirm.DeliveryTag)
+		}
+	}()
 
 	qu, err := ch.Ch.QueueDeclare(queue, true, false, false, false, nil)
 	if err != nil {
